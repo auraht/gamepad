231030205168a54754085837ff1b6cd158911c6d
KennyTM~ <kennytm@gmail.com>
2011-01-13 16:47:47 +0800

    Added limitations of this library; Transfer copyright to auraHT.

--- windows/GamepadChangedObserver_Windows.cpp
+++ windows/GamepadChangedObserver_Windows.cpp
@@ -3,20 +3,20 @@
 GamepadChangedObserver_Windows.cpp ... Implementation of GamepadChangedObserver
 for Windows.
 
-Copyright (c) 2011  KennyTM~ <kennytm@gmail.com>
+Copyright (c) 2011  aura Human Technology Ltd.  <rnd@auraht.com>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
 * Redistributions of source code must retain the above copyright notice, this
-list of conditions and the following disclaimer.
+  list of conditions and the following disclaimer.
 * Redistributions in binary form must reproduce the above copyright notice, 
-this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
-* Neither the name of the KennyTM~ nor the names of its contributors may be
-used to endorse or promote products derived from this software without
-specific prior written permission.
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+* Neither the name of "aura Human Technology Ltd." nor the names of its
+  contributors may be used to endorse or promote products derived from this
+  software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
